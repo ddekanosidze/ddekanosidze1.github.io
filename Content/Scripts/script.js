ec98950e48548450f2515f18c2928dda02dcfdc3
ddekanosidze <44446303+ddekanosidze@users.noreply.github.com>
2019-11-13 06:14:57 -0800

    Add files via upload

--- Content/Scripts/script.js
+++ Content/Scripts/script.js
@@ -10,7 +10,39 @@ function menuFunction(){
         $(".open-menu").addClass("none");
     });
     $(".my-history-button").click(function(){
-        $(".history").removeClass("none");
+        $(".history").removeClass("none");       
+    });
+    $(".close-history").click(function(){
+        $(".history").addClass("none");
+    });
+    $(".last-houndreed-button").click(function(){
+        $(".last-houndreed-numbers-container").removeClass("none");
+        $(".user-game-history").addClass("none");
+        $(".game-history-button").css({"color":"white","border":"transparent" })
+        $(".last-houndreed-button").addClass("active");  
+        $(".last-houndreed-button").css({"border-bottom":"10px solid #bfae4b","color":"#bfae4b" });
+
+    });
+    $(".game-history-button").click(function(){
+        $(".last-houndreed-numbers-container").addClass("none");
+        $(".user-game-history").removeClass("none");
+        $(".last-houndreed-button").css({"color":"white","border":"transparent" })
+        $(".game-history-button").addClass("active");
+        $(".game-history-button").css({"border-bottom":"10px solid #bfae4b","color":"#bfae4b" });
+
+    });
+    $(".last-houndreed").click(function(){
+        $(".open-menu").removeClass("none");
+        $(".history").removeClass("none"); 
+        $(".user-game-history").addClass("none");
+        $(".last-houndreed-numbers-container").removeClass("none");
+        $(".game-history-button").css({"color":"white","border":"transparent" })
+        $(".last-houndreed-button").css({"border-bottom":"10px solid #bfae4b","color":"#bfae4b" });
+            
+            $(".close-history").click(function(){
+              $(".history").addClass("none");
+              $(".open-menu").addClass("none");
+            });
         
-    })
+    });
 }
